7d0bdb807064e044730dc97e98424da48b346600
Irina-Koida <91370760+Irina-Koida@users.noreply.github.com>
2021-10-21 15:34:42 +0300

    add some methods

--- ConsoleApp1/ConsoleApp1/Calc.cs
+++ ConsoleApp1/ConsoleApp1/Calc.cs
@@ -8,6 +8,48 @@ namespace ConsoleApp1
 {
    public static class Calc
    {
+        public static double Plus(double x, double y)
+        {
+            return x + y;
+        }
 
+        public static double Minus(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Division(double x, double y)
+        {
+            while(y == 0)
+            {
+                Console.WriteLine("Error");
+                return 0;
+            }
+            return x / y;
+        }
+
+        public static double RestOfDivide(double x, double y)
+        {
+            return x % y;
+        }
+
+        static public double NumInput()
+        {
+            string numberString = Console.ReadLine();
+            bool isDoubleInput = double.TryParse(numberString, out double numberDouble);
+            while (!isDoubleInput)
+            {
+                Console.WriteLine("Eror");
+                numberString = Console.ReadLine();
+                isDoubleInput = double.TryParse(numberString, out  numberDouble);
+            }
+            return numberDouble;
+        }
    }
+
 }
